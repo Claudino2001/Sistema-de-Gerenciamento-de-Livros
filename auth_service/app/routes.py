b7e8e9f236da815a79bc346ed078563ca0dd8e30
Gabriel Claudino <gaclaudino.13@gmail.com>
2024-06-13 10:36:09 -0300

    view/list and add funcionando

--- auth_service/app/routes.py
+++ auth_service/app/routes.py
@@ -1,26 +1,33 @@
-from flask import request, jsonify
+from flask import request, jsonify, render_template, redirect, url_for
 from flask_jwt_extended import create_access_token, jwt_required
 from .models import User, db
 
 def init_routes(app):
     @app.route('/')
     def index():
-        return 'Welcome to the Auth Service!'
+        return render_template('index.html')
 
-    @app.route('/auth/register', methods=['POST'])
-    def register():
-        data = request.get_json()
-        new_user = User(username=data['username'], password=data['password'])
-        db.session.add(new_user)
-        db.session.commit()
-        return jsonify(message="User created"), 201
-
-    @app.route('/auth/login', methods=['POST'])
+    @app.route('/login', methods=['GET', 'POST'])
     def login():
-        data = request.get_json()
-        user = User.query.filter_by(username=data['username']).first()
-        if user and user.password == data['password']:
-            access_token = create_access_token(identity={'username': user.username})
-            return jsonify(access_token=access_token), 200
-        else:
-            return jsonify(message="Invalid credentials"), 401
+        if request.method == 'POST':
+            data = request.form
+            user = User.query.filter_by(username=data['username']).first()
+            if user and user.password == data['password']:
+                access_token = create_access_token(identity={'username': user.username})
+                # Redirecionar para o serviço de livros com o token como parâmetro na URL
+                return redirect(f'http://127.0.0.1:5002/?token={access_token}')
+            else:
+                return 'Invalid credentials', 401
+        return render_template('login.html')
+
+    @app.route('/register', methods=['GET', 'POST'])
+    def register():
+        if request.method == 'POST':
+            data = request.form
+            new_user = User(username=data['username'], password=data['password'])
+            db.session.add(new_user)
+            db.session.commit()
+            access_token = create_access_token(identity={'username': new_user.username})
+            # Redirecionar para o serviço de livros com o token como parâmetro na URL
+            return redirect(f'http://127.0.0.1:5002/?token={access_token}')
+        return render_template('register.html')
--- books_service/app/routes.py
+++ books_service/app/routes.py
@@ -1,46 +1,81 @@
-from flask import request, jsonify
-from flask_jwt_extended import jwt_required
+from flask import request, jsonify, render_template, redirect, url_for
+from flask_jwt_extended import jwt_required, decode_token, get_jwt_identity
 from .models import Book, db
 
 def init_routes(app):
     @app.route('/')
     def index():
-        return 'Welcome to the Books Service!'
+        token = request.args.get('token')
+        if token:
+            try:
+                decoded_token = decode_token(token)
+                return render_template('index.html', token=token)
+            except:
+                return 'Invalid or expired token', 401
+        else:
+            return 'Token is required', 400
 
     @app.route('/books', methods=['GET'])
     def get_books():
-        books = Book.query.all()
-        return jsonify([{'id': book.id, 'title': book.title, 'author': book.author} for book in books]), 200
+        token = request.args.get('token')
+        if token:
+            try:
+                decoded_token = decode_token(token)
+                books = Book.query.all()
+                return render_template('books.html', books=books, token=token)
+            except:
+                return 'Invalid or expired token', 401
+        else:
+            return 'Token is required', 400
 
-    @app.route('/books', methods=['POST'])
-    @jwt_required()
+    @app.route('/add_book', methods=['GET', 'POST'])
     def add_book():
-        data = request.get_json()
-        new_book = Book(title=data['title'], author=data['author'])
-        db.session.add(new_book)
-        db.session.commit()
-        return jsonify(message="Book added"), 201
-
-    @app.route('/books/<int:id>', methods=['PUT'])
-    @jwt_required()
-    def update_book(id):
-        data = request.get_json()
-        book = Book.query.get(id)
-        if book:
-            book.title = data['title']
-            book.author = data['author']
-            db.session.commit()
-            return jsonify(message="Book updated"), 200
+        token = request.args.get('token')
+        if token:
+            try:
+                decoded_token = decode_token(token)
+                if request.method == 'POST':
+                    data = request.form
+                    new_book = Book(title=data['title'], author=data['author'])
+                    db.session.add(new_book)
+                    db.session.commit()
+                    return redirect(url_for('get_books', token=token))
+                return render_template('add_book.html', token=token)
+            except:
+                return 'Invalid or expired token', 401
         else:
-            return jsonify(message="Book not found"), 404
+            return 'Token is required', 400
 
-    @app.route('/books/<int:id>', methods=['DELETE'])
-    @jwt_required()
+    @app.route('/edit_book/<int:id>', methods=['GET', 'POST'])
+    def edit_book(id):
+        token = request.args.get('token')
+        if token:
+            try:
+                decoded_token = decode_token(token)
+                book = Book.query.get(id)
+                if request.method == 'POST':
+                    data = request.form
+                    book.title = data['title']
+                    book.author = data['author']
+                    db.session.commit()
+                    return redirect(url_for('get_books', token=token))
+                return render_template('edit_book.html', book=book, token=token)
+            except:
+                return 'Invalid or expired token', 401
+        else:
+            return 'Token is required', 400
+
+    @app.route('/delete_book/<int:id>', methods=['POST'])
     def delete_book(id):
-        book = Book.query.get(id)
-        if book:
-            db.session.delete(book)
-            db.session.commit()
-            return jsonify(message="Book deleted"), 200
+        token = request.args.get('token')
+        if token:
+            try:
+                decoded_token = decode_token(token)
+                book = Book.query.get(id)
+                db.session.delete(book)
+                db.session.commit()
+                return redirect(url_for('get_books', token=token))
+            except:
+                return 'Invalid or expired token', 401
         else:
-            return jsonify(message="Book not found"), 404
+            return 'Token is required', 400
